bcdd2eb378ed9a142b561c42320f03faefb1eb9b
zeki can eksi <zekicaneksi@gmail.com>
2022-09-10 13:42:20 +0300

    actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # YouTube-Subscription-Feed-Manager
 A site that ables you to filter and see better your subscription feed.
+
---
+++ youtube-subscription-feed-manager/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import styles from '../styles/indexPage.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Signin - Subs Feed Manager</title>
+      </Head>
+      <div className={styles.container}>
+        <p>supp</p>
+      </div>
+    </>
+  )
+}
