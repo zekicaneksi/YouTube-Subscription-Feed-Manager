43b467bb15669294434b4335bc5d57b8f3d04564
zeki can eksi <zekicaneksi@gmail.com>
2022-09-12 15:26:30 +0300

    login and logout

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,94 @@ dotenv.config({ path: path.join(__dirname, `.env.${process.env.NODE_ENV}`) });
 
 const app = express();
 
-app.get('/test', async (req,res) => {
-  console.log('a request has been made');
-  return res.send('hello from server');
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_ADDRESS);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+  next();
+});
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false ,
+  saveUninitialized: true ,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());  
+
+var GoogleStrategy = googleAuth.Strategy;
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    profile.refreshToken=refreshToken;
+    return cb(null, profile);
+  }
+));
+
+passport.serializeUser( (user, done) => { done(null, user)} );
+passport.deserializeUser((user, done) => {done (null, user)});
+
+// Middleware
+function checkAuthenticated (req, res, next) {
+  if (!req.isAuthenticated()) return res.status(401).send(JSON.stringify('/auth/google'));
+  return next();
+}
+
+app.get('/checkAuthenticated', checkAuthenticated, async (req, res) => {
+  return res.status(200).send();
+});
+
+app.get('/auth/google', (req, res, next) => {
+  if (req.query.return) {req.session.oauth2return = req.query.return;}
+  next();
+  },
+  passport.authenticate('google', { accessType:'offline', prompt:'consent', scope: [
+    'https://www.googleapis.com/auth/youtube.readonly',
+    "profile"] })
+);
+
+app.get(process.env.CALLBACK_URL,
+  function (req, res, next) {
+    if(req.session.oauth2return) req.oauth2return = req.session.oauth2return;
+    next();
+  },
+  passport.authenticate('google'),
+  function(req, res) {
+    return res.redirect(req.oauth2return);
+  }
+);
+
+app.get('/logout', checkAuthenticated, async (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+    return next(err);
+    }
+    return res.status(200).send();
+  });
+  return res.status(500).send();
+});
+
+app.get('/getAcessToken', checkAuthenticated, async (req,res) => {
+  let toReturn='';
+  await Axios
+  .post('https://oauth2.googleapis.com/token', {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'refresh_token',
+    refresh_token: req.user.refreshToken
+  })
+  .then(res => {
+    toReturn = res.data.access_token;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  return res.send(JSON.stringify(toReturn));
 });
 
 app.listen(process.env.PORT, () => console.log(`Server listening on port: ${process.env.PORT}`));
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,45 @@
 import Head from 'next/head';
-import styles from '../styles/indexPage.module.css';
-
-function test(){
-  fetch('/api/test')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-}
+import { useEffect, useState } from 'react';
+import styles from '../styles/page_index.module.css';
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(true);
+
+  function checkAuthenticated(){
+    fetch(process.env.NEXT_PUBLIC_BACKEND_ADDRESS+'/checkAuthenticated', { mode: 'cors', credentials: 'include'})
+    .then((response) => {
+      if(response.status === 401){
+        response.json().then((data) => {
+          window.location.replace(process.env.NEXT_PUBLIC_BACKEND_ADDRESS+ data+'?return='+ encodeURIComponent(window.location.origin + window.location.pathname));
+        });
+      } else {
+        setLoading(false);
+      }
+    });
+  }
+
+  function logout(){
+    fetch(process.env.NEXT_PUBLIC_BACKEND_ADDRESS+'/logout', { mode: 'cors', credentials: 'include'})
+    .then((response) => {window.location.replace('/')});
+  }
+
+  useEffect(() => {
+    checkAuthenticated();
+  },[]);
+
+  if(loading){
+    return (<p>Loading...</p>);
+  }
+
   return (
     <>
       <Head>
-        <title>Signin - Subs Feed Manager</title>
+        <title>Subs Feed Manager</title>
       </Head>
       <div className={styles.container}>
-        <p>supp</p>
-        <button onClick={test}>test backend (check console)</button>
+        <p>Hello</p>
+        <button onClick={logout}>Logout</button>
       </div>
     </>
   )
